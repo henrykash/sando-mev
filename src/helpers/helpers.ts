f6be0e09a37131019691977ecfa22cb540cc54a1
henrykash <henrykariuki003@gmail.com>
2023-07-03 18:44:28 +0300

    implemented errorHandling class

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,7 @@
+// Globals
+import { createRequire } from "module";
+import dotenv from "dotenv";
+import { ethers } from "ethers";
+const customRequire = createRequire(import.meta.url);
+
+dotenv.config();
